ab84b0be0479b7c9788ff1bbf4aa917f6beb91ce
aacgectyuoki <maxdell-thibodeau@hotmail.com>
2024-11-12 14:27:21 -0500

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DefaultPage from './components/DefaultPage'; // Adjust path based on folder structure
 import AboutPage from './components/AboutPage';
 import ScrollToTop from './components/ScrollToTop';
