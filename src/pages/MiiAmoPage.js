4f7bb8489fd49e0b33fe666ea1b7c2bcaa0a43d6
aacgectyuoki <maxdell-thibodeau@hotmail.com>
2025-01-07 16:30:22 -0500

    MiiAmo

--- src/pages/MiiAmoPage.js
+++ src/pages/MiiAmoPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
-import umami from '../assets/images/Umami_Image_PRIMARY_IMAGE.jpg';
-import umami01 from '../assets/images/Umami_Image_01.jpg';
-import umami02 from '../assets/images/Umami_Image_02.jpg';
-import umami03 from '../assets/images/Umami_Image_03.jpg';
-import umami04 from '../assets/images/Umami_Image_04.jpg';
-import umami05 from '../assets/images/Umami_Image_05.jpg';
-import umami06 from '../assets/images/Umami_Image_06.jpg';
+import miiamo from '../assets/images/SymbolTM_MiiAmo_PRIMARY_IMAGE.png';
+import miiamo01 from '../assets/images/SymbolTM_MiiAmo_01.png';
+import miiamo02 from '../assets/images/SymbolTM_MiiAmo_02.png';
+import miiamo03 from '../assets/images/SymbolTM_MiiAmo_03.png';
+import miiamo04 from '../assets/images/SymbolTM_MiiAmo_04.png';
+import miiamo05 from '../assets/images/SymbolTM_MiiAmo_05.png';
+import miiamo06 from '../assets/images/SymbolTM_MiiAmo_06.png';
 
 const MiiAmoPage = () => {
   return (
@@ -18,12 +18,12 @@ const MiiAmoPage = () => {
         className="mx-auto px-6 mt-8 w-11/12"
         style={{ maxWidth: '1400px' }}
       >
-        <h1 className="text-4xl font-bold mb-2">SBE x Umami Burger</h1>
+        <h1 className="text-4xl font-bold mb-2">Mii Amo</h1>
         <div className="h-2"></div>
         {/* <p className="text-lg text-gray-600 mb-4">Our Insights. Your Breakthroughs.</p> */}
         <img
-          src={umami}
-          alt="Umami Branding"
+          src={miiamo}
+          alt="Mii Amo Branding"
           className="w-full shadow-lg"
         />
       </section>
@@ -36,43 +36,33 @@ const MiiAmoPage = () => {
         <div className="mb-8">
           <h2 className="text-2xl font-semibold mb-2">Background</h2>
           <p className="text-gray-700">
-          Before the COVID-19 pandemic upended the restaurant industry, 
-          SBE Entertainment Group began developing chef-driven delivery-only brands to tap into the growing food on-demand segment. 
-          <br></br><br></br>
-          SBE Entertainment Group supports their hotels by turning dark restaurant kitchens into “ghost kitchens.” 
-          That’s always been part of the company’s strategy. There will be a greater void if more restaurants close 
-          permanently due to the pandemic.
-          <br></br><br></br>
-          “It's a solution that will generate revenue in a manner that can deal with the demand that we see today during 
-          COVID-19 and the demand that we're going to see post COVID.” Nazarian said.
+          Mii amo is a luxury spa collection within Enchantment Group. It is considered one of the top 10 throughout the country.
           </p>
         </div>
 
         <div className="mb-8">
           <h2 className="text-2xl font-semibold mb-2">Action</h2>
           <p className="text-gray-700">
-          Umami Burger is one of SBE’s growing sub-brands. In order for the sub-brand to grow, they were in need of consistent guidelines in their communications.
-          <br></br><br></br>
-          I created the menus, billboards, posters and graphic stickers.
+          For more than 4 years, I have worked together for Mii amo.
           </p>
         </div>
 
         <div className="mb-8">
           <h2 className="text-2xl font-semibold mb-2">Deliverables</h2>
           <p className="text-gray-700">
-          Design, Signage, Environment, Marketing Communications
+          Online advertising and customer communications.
           </p>
         </div>
 
         {/* Image Gallery */}
         <div className="mb-8">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <img src={umami01} alt="Umami Branding" className="shadow-lg" />
-            <img src={umami02} alt="Umami Branding" className="shadow-lg" />
-            <img src={umami03} alt="Umami Branding" className="shadow-lg" />
-            <img src={umami04} alt="Umami Branding" className="shadow-lg" />
-            <img src={umami05} alt="Umami Branding" className="shadow-lg" />
-            <img src={umami06} alt="Umami Branding" className="shadow-lg" />
+            <img src={miiamo01} alt="Mii Amo Branding" className="shadow-lg" />
+            <img src={miiamo02} alt="Mii Amo Branding" className="shadow-lg" />
+            <img src={miiamo03} alt="Mii Amo Branding" className="shadow-lg" />
+            <img src={miiamo04} alt="Mii Amo Branding" className="shadow-lg" />
+            <img src={miiamo05} alt="Mii Amo Branding" className="shadow-lg" />
+            <img src={miiamo06} alt="Mii Amo Branding" className="shadow-lg" />
           </div>
         </div>
 
@@ -80,12 +70,12 @@ const MiiAmoPage = () => {
         <div className="mb-8">
           <h2 className="text-xl font-bold mb-2">Learn More</h2>
           <a
-            href="https://www.sbe.com/restaurants/umami-burger.com"
+            href="https://www.miiamo.com"
             target="_blank"
             rel="noopener noreferrer"
             className="text-blue-600 hover:underline"
           >
-            www.sbe.com/restaurants/umami-burger.com
+            www.miiamo.com
           </a>
           <br></br>
           ROLE: Branding, Design, and Strategy
